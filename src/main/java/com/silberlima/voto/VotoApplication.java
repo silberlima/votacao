93fab6e4c52a23a0ea39700362372ee60be87868
silberlima <43426080+silberlima@users.noreply.github.com>
2026-02-27 16:02:20 -0300

    Initial commit

---
+++ src/main/java/com/silberlima/voto/VotoApplication.java
@@ -0,0 +1,13 @@
+package com.silberlima.voto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silberlima/voto/VotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silberlima.voto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
