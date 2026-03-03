0e75e89b57ac674d5a1a6c528fe7142c8dd46778
silberlima <43426080+silberlima@users.noreply.github.com>
2026-03-03 12:02:37 -0300

    fix: Versão Kafka

--- src/main/java/com/silberlima/voto/domain/repository/VotoRepository.java
+++ src/main/java/com/silberlima/voto/domain/repository/VotoRepository.java
@@ -5,8 +5,6 @@ import com.silberlima.voto.domain.model.VotoEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface VotoRepository extends JpaRepository<Voto, Long> {
     boolean existsByPautaIdAndAssociadoId(Long pautaId, String associadoId);
